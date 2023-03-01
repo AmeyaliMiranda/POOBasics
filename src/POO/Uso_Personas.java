43617aec77db0ae86599f366e5ce6e68f60c0ab2
Ameyali Miranda <ame.gm1806@gmail.com>
2023-02-28 20:05:52 -0600

    POO Abstracción y encapsulamiento

--- src/POO/Uso_Personas.java
+++ src/POO/Uso_Personas.java
@@ -7,6 +7,17 @@ public class Uso_Personas {
     }
 }
 
+abstract class Persona{
+    private String nombre;
+    public Persona(String nom){
+        nombre=nom;
+    }
+    public String dameNombre(){
+        return nombre;
+    }
+    public abstract String dameDescripcion();
+}
+
 
 
 
