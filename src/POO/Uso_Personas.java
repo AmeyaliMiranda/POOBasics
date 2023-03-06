c002ac3657ed2e6079c8958c4f2e9e00173266a7
Apokochito <dpinto1@ucol.mx>
2023-03-05 18:23:40 -0600

    First Diana's review

--- src/POO/Uso_Personas.java
+++ src/POO/Uso_Personas.java
@@ -1,51 +1,84 @@
 package POO;
 
 public class Uso_Personas {
-    public static void main(String[] args){
-        Persona[] lasPersonas=new Persona [2];
+    public static void main(String[] args) {
+        Persona[] personas = new Persona[2]; // Más entendible y simplificado
 
-        lasPersonas[0]=new Empleado("Ameyali", 5000);
-        lasPersonas[1]=new Alumno("Alfonso", "Programación");
+        personas[0] = new Empleado("Ameyali", 5000, "El payaso tiene un sueldo de: "); // Overriding polymorphism
+        //personas[1] = new Alumno("Alfonso", "Programación"); // Overriding polymorphism
 
-        for (Persona p:lasPersonas) {
-            System.out.println(p.dameNombre() + ", " + p.dameDescripcion() );
+        // Utilizar la palabra reservada: this
+        // Improve getter and setters (add more setters)
+        // Add overloading polymorphism example
+        // Change to english language
+        // Review all IntelliJ corrections
+        // Gitignore for Java
+
+        for (Persona p : personas) {
+            System.out.println(p.getNombre() + ", " + p.dameDescripcion());
         }
     }
 }
 
-abstract class Persona{
+abstract class Persona {
     private String nombre;
-    public Persona(String nom){
-        nombre=nom;
+    String description;
+
+    Persona(String nom, String description) {
+        this.nombre = nom;
+        this.description = description;
     }
-    public String dameNombre(){
+
+    public String getNombre() {
         return nombre;
     }
-    public abstract String dameDescripcion();
+
+    public abstract String dameDescripcion(); // Abstracta por definición, abstraemos la -implementación-
+
+    public abstract void setDescription(String description);
 }
-class Empleado extends Persona{
-    private String nombre;
+
+class Empleado extends Persona {
     private double sueldo;
-    public Empleado(String nom, double sue){
-        super(nom);
-        sueldo=sue;
+
+    Empleado(String nom, double sue, String customDescription) {
+        super(nom, "El empleado tiene un sueldo de: ");
+        this.sueldo = sue;
+        this.description = customDescription;
     }
 
-    public String dameDescripcion(){
-        return "El empleado tiene un sueldo de: " + sueldo;
+    public String dameDescripcion() {
+        // Getter
+        return description + sueldo;
     }
-    public double dameSueldo(){
 
+    @Override
+    public void setDescription(String description) {
+        // Setter
+        this.description = description;
+    }
+
+    public double dameSueldo() {
         return sueldo;
     }
 }
-class Alumno extends Persona{
+
+class Alumno extends Persona {
     private String carrera;
-    public  Alumno (String nom, String car){
-        super(nom);
-        carrera=car;
+
+    public Alumno(String nom, String car) {
+        super(nom, "Este alumno está estudiando la carrera de: ");
+        carrera = car;
     }
-    public String dameDescripcion(){
+
+    // This need to be changed
+    public String dameDescripcion() {
         return "Este alumno está estudiando la carrera de: " + carrera;
+    }
+
+    @Override
+    public void setDescription(String description) {
+        // Setter
+        this.description = description;
     }
 }
