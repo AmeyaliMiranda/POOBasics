a04b615eb602d91a196984484b1271c380dd8585
Ameyali Miranda <ame.gm1806@gmail.com>
2023-03-11 16:03:39 -0600

    CStart Project Correction

--- src/POO/Uso_Personas.java
+++ src/POO/Uso_Personas.java
@@ -2,50 +2,17 @@ package POO;
 
 public class Uso_Personas {
     public static void main(String[] args){
-        Persona[] lasPersonas=new Persona [2];
 
-        lasPersonas[0]=new Empleado("Ameyali", 5000);
-        lasPersonas[1]=new Alumno("Alfonso", "Programación");
-
-        for (Persona p:lasPersonas) {
-            System.out.println(p.dameNombre() + ", " + p.dameDescripcion() );
-        }
     }
 }
 
-abstract class Persona{ //abstraccion
-    private String nombre; //encapsulamiento
-    public Persona(String nom){
-        nombre=nom;
-    }
-    public String dameNombre(){
-        return nombre;
-    }
-    public abstract String dameDescripcion();
+abstract class Persona{
+
 }
-class Empleado extends Persona{ //herencia y polimorfismo
-    private String nombre;
-    private double sueldo;
-    public Empleado(String nom, double sue){
-        super(nom);
-        sueldo=sue;
-    }
+class Empleado extends Persona{
 
-    public String dameDescripcion(){
-        return "El empleado tiene un sueldo de: " + sueldo;
-    }
-    public double dameSueldo(){
 
-        return sueldo;
-    }
 }
 class Alumno extends Persona{
-    private String carrera;
-    public  Alumno (String nom, String car){
-        super(nom);
-        carrera=car;
-    }
-    public String dameDescripcion(){
-        return "Este alumno está estudiando la carrera de: " + carrera;
-    }
+
 }
